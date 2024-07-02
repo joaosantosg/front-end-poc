0ac19ae7922545523afb6bb90df7c5f05b5b3d21
Urubu do pix <urubudopix@gmail.com>
2024-07-02 16:05:47 -0300

    .

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import '../App.css';
 
 function LoginPage() {
-  const [email, setEmail] = useState('teste@exemplo.com');
-  const [password, setPassword] = useState('senha123');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
